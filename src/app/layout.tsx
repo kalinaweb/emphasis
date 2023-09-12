3a47b0c25fa78f6cdad80056bae305828f064078
Valentina Myatina <kalina_web@mail.ru>
2023-09-12 14:35:54 +0300

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Ударения для ЕГЭ',
   description: 'Игра для заучивания правильного ударения в словах',
+  icons: {
+    icon: 'favicon.ico',
+  },
 }
 
 export default function RootLayout({
